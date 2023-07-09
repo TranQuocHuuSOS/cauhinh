b9ea423e5190415912fd186ea1c571ec51eb5b7d
TranQuocHuuSOS <0tranquochuu02003@gmail.com>
2023-07-09 12:08:15 +0700

    làm app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
-import routes from "./routes";
-function App() {
-  return (
-    <div className="App">
-      <routes></routes>
-    </div>
-  );
-}
-
-export default App;
+import React, {Component} from 'react';											
+import './App.css';											
+import AllProduct from './../src/component/layout_page/AllProduct';											
+import { BrowserRouter as Router, Route } from 'react-router-dom';											
+import routes from './routes.js';											
+import { ToastContainer, toast } from 'react-toastify';											
+import 'react-toastify/dist/ReactToastify.css';											
+class App extends Component {											
+  render(){											
+  return (											
+    <Router>											
+    <ToastContainer />											
+      <Route>  											
+        {this.showContentMenu(routes)} 											
+      </Route>    											
+    </Router>  											
+    );											
+                      
+  }											
+  showContentMenu = (routes) =>{											
+    var result = null;											
+    if (routes.length > 0) {											
+      result = routes.map((route, index) =>{											
+        return (											
+            <Route key ={index} path = {route.path} exact = {route.exact} component={route.main} />											
+          );											
+      });											
+    }											
+    return result;											
+  }											
+                      
+}											
+                      
+export default App;											
